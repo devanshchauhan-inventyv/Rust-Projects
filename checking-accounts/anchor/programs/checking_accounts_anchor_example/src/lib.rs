85a1ea21d7e1df451ff54a96e0dfcb6042805216
devanshchauhan <devanshchauhaninventyv@gmail.com>
2026-03-16 18:35:47 +0530

    Add:anchor project for checking-accounts

---
+++ checking-accounts/anchor/programs/checking_accounts_anchor_example/src/lib.rs
@@ -0,0 +1,20 @@
+use anchor_lang::prelude::*;
+
+declare_id!("8fZ5pRK72cRGdQEfZiKftJinGA6LrS6GQCLZR3trXpch");
+
+// #[program]
+// pub mod anchor {
+//     use super::*;
+
+//     pub fn initialize(ctx: Context<CheckingAccounts>) -> Result<()> {
+//         msg!("Greetings from: {:?}", ctx.program_id);
+//         Ok(())
+//     }
+// }
+
+// #[derive(Accounts)]
+// pub struct CheckingAccounts<'info> {
+//     #[account]
+//     payer:Signer<'info>,
+//     accounts_to_create
+// }
